1276c77325147bbbd3d9990e5fcf8cfa629a5222
Erland Ranvinge <erland.ranvinge@valtech.se>
2013-05-27 09:07:59 +0200

    Initial commit

---
+++ wsdlget/Arguments.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wsdlget
+{
+	public class Arguments
+	{
+		private readonly Dictionary<string, string> _pairs = new Dictionary<string, string>();
+		private const string SwitchPrefix = "-";
+		public Arguments(string[] args)
+		{
+			for (var i = 0; i < args.Length; i++)
+			{
+				var current = args[i];
+				if (current.StartsWith(SwitchPrefix))
+				{
+					current = current.Remove(0, 1);
+					var next = (i + 1) < args.Length ? args[i + 1] : "";
+					if (!next.StartsWith(SwitchPrefix))
+					{
+						_pairs[current] = next;
+						i++;
+					}
+					else
+					{
+						_pairs[current] = "";
+					}
+					continue;
+				}
+				_pairs[""] = current;
+			}
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder(1014);
+			foreach (var pair in _pairs)
+				sb.AppendFormat("'{0}'='{1}' ", pair.Key, pair.Value);
+			return sb.ToString();
+		}
+
+		public string this[string key]
+		{
+			get { return _pairs.ContainsKey(key) ? _pairs[key] : ""; }
+		}
+
+		public bool SwitchSet(string key)
+		{
+			return _pairs.ContainsKey(key);
+		}
+	}
+}
---
+++ wsdlget/HttpHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Net;
+
+namespace wsdlget
+{
+	public class HttpHelper
+	{
+		public static string GetUrl(string url)
+		{
+			var request = WebRequest.Create(url);
+			var response = request.GetResponse();
+			var dataStream = response.GetResponseStream();
+			var reader = new StreamReader(dataStream);
+			return reader.ReadToEnd();			
+		}
+
+		public static string GetDocumentName(string url)
+		{
+			var index = url.LastIndexOf('/') + 1;
+			if (index < 0)
+				return "";
+			var document = url.Substring(index);
+			index = document.IndexOf("?");
+			if (index < 0)
+				return document;
+			return Path.GetFileNameWithoutExtension(document.Substring(0, index));
+		}
+	}
+}
